dd448777ad012a286e517bb874dab8fbfafaf4a9
sahin Islam <sahinislam878@gmail.com>
2025-12-25 20:38:49 +0530

    SQL 4: CRUD 2

--- SQL_Codes.sql
+++ SQL_Codes.sql
@@ -223,3 +223,137 @@ Truncate table students;
 
 	-- Drop : 
 drop table students;
+
+
+-- SQL 4: CRUD 2 / Lab Session 2
+    
+	-- Limit and Offset
+
+use sakila;
+select * from film
+limit 2
+offset 99;
+
+select * from film
+order by rental_duration desc
+limit 5;
+		
+	-- Like Keyword : 
+		-- Problem Statement : Get all the movies which have love in it.
+
+select film_id,title, description
+from film
+where binary title  like '%LOVE%';
+
+
+
+	-- Working with Null
+    
+
+select 1 = 1;
+select 1 = 2;
+
+select 1 = Null;
+select Null = Null;
+
+select Null is Null;
+
+select Null is not Null;    
+
+
+
+	-- Concate and Round Function
+    
+    use school;
+    select concat(first_name,' ' ,last_name) from students;
+    
+    
+    select round(12.36652545,2);
+
+
+
+	-- Problem 1: Identifying Popular Films
+	-- The management wants to identify the first 10 distinct film titles from the film table that contain the word "Adventure"
+	-- in their title (case insensitive), sorted alphabetically. Write an SQL query that returns the film titles.
+
+
+use sakila;
+select distinct title from film
+where  title like '%Adventure%'
+order by title 
+limit 10;
+
+
+	-- Problem 2: Tracking Unreturned Rentals
+	-- The store is experiencing issues with customers not returning their rentals on time. From the rental table, list the first 5 rental IDs 
+    -- where the return_date is NULL, ordered by the rental_date in descending order. Write an SQL query to fetch this data.
+
+select rental_id from rental
+where return_date is null
+order by rental_id
+limit 5;
+
+
+
+	-- Problem 3: Cleaning Up Missing Data
+	-- The IT department identified that some film descriptions in the film table are missing. To prepare a list for further investigation, 
+    -- retrieve 15 distinct film titles with NULL descriptions, ordered by their title in ascending order. Write an SQL query to achieve this.
+
+
+use sakila;
+
+
+select distinct title from film
+where description is null
+order by title asc
+limit 15;
+
+
+
+	-- Problem 4: Filtering by Customer Names
+	-- The marketing team wants to run a campaign for customers whose names start with the letter "A" or contain "Smith" anywhere in their name.
+    -- From the customer table, fetch the first 10 customers (first name and last name) whose names match this criterion, sorted by last_name in alphabetical order.
+    -- Write an SQL query that satisfies this requirement.
+
+select concat(first_name, ' ', last_name) as name from customer
+where first_name like 'A%' or
+ first_name like '%Smith%' or
+last_name like '%Smith%'
+order by last_name asc
+limit 10;
+
+
+	-- Problem 5: Detecting Unused Inventory
+	-- The management suspects that some inventory items in the inventory table might not be linked to any store.
+    -- Generate a report to identify the first 10 inventory items where the store_id is NULL, 
+	-- ordered by inventory_id in ascending order. Write an SQL query to provide the required information.
+
+select * from inventory
+where store_id is null
+order by inventory_id asc
+limit 10;
+
+
+
+	-- Problem 6: Exploring Film Ratings
+	-- To understand customer preferences, the management wants to know the distinct film ratings from the film table where special_features is NULL. 
+    -- Return the ratings sorted in ascending order. Write an SQL query for this.
+    use sakila;
+    select distinct rating from film
+    where special_features is null 
+    order by rating asc;
+
+	-- Problem 7: Identifying the Second Best Movie
+	-- You are tasked with identifying the "second- best" movie in the Sakila database. 
+    -- The ranking is determined by the rental rate, rental duration, and length. Management has specified the following rules for ranking the movies:
+	-- First, rank the movies by rental_rate in descending order (lower is better).
+	-- If there’s a tie, compare by rental_duration in descending order (longer is better).
+	-- If there’s still a tie, compare by length in descending order (longer is better).
+
+select * from film
+order by rental_rate desc,
+rental_duration desc, 
+length desc
+
+limit 1
+offset 1;
